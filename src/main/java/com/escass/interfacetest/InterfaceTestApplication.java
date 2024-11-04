d3ce67050e8cd8eec7feb9745168d0dbfbc93a8d
escurse <av1967@naver.com>
2024-11-05 08:52:28 +0900

    interfaceTest

---
+++ src/main/java/com/escass/interfacetest/InterfaceTestApplication.java
@@ -0,0 +1,13 @@
+package com.escass.interfacetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escass/interfacetest/InterfaceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escass.interfacetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
