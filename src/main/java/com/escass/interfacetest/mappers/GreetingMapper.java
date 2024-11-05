bd7f8375f00b38e8661c123f5960e294bdab39b1
escurse <av1967@naver.com>
2024-11-05 10:02:35 +0900

    interfaceTest

--- src/main/java/com/escass/interfacetest/mappers/GreetingMapper.java
+++ src/main/java/com/escass/interfacetest/mappers/GreetingMapper.java
@@ -2,11 +2,8 @@ package com.escass.interfacetest.mappers;
 
 import com.escass.interfacetest.entities.GreetingEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface GreetingMapper {
     int insertGreeting(GreetingEntity greet);
-
-    GreetingEntity selectGreetingByIndex(@Param("index") int index);
 }
