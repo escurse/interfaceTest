95deab52a7a1bd2b432836f6b478b7bc7cfffec0
escurse <av1967@naver.com>
2024-11-05 09:16:00 +0900

    interfaceTest

---
+++ src/main/java/com/escass/interfacetest/entities/greetEntity.java
@@ -0,0 +1,17 @@
+package com.escass.interfacetest.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"index"})
+public class greetEntity {
+    private int index;
+    private String writer;
+    private String content;
+    private LocalDateTime createdAt;
+}
